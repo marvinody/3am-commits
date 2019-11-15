e9b8c7b00d119c64894835d41ab7085e8dc56499
Manuel Bugallo <marvinody@gmail.com>
2019-11-15 00:33:24 -0500

    feat: creates redux store for commits with firebase

---
+++ src/store/commits.js
@@ -0,0 +1,28 @@
+import firebase from '../firebase';
+const LOAD_PICS = 'LOAD_PICS'
+
+
+export const getCommits = () => async dispatch => {
+  const db = firebase.firestore();
+  const commitsRef = db.collection('commits');
+  const snapshot = await commitsRef.limit(20).get()
+  let commits = []
+  snapshot.forEach(doc =>
+    commits = [...commits, doc.data()]
+  )
+  dispatch({
+    type: LOAD_PICS,
+    commits,
+  })
+}
+
+const initialState = []
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_PICS:
+      return [...state, ...action.commits]
+    default:
+      return state
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import game from './game';
-export * from './game';
+import commits from './commits';
+export * from './commits';
 
-const reducer = combineReducers({ game })
+const reducer = combineReducers({ commits })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
