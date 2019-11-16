1c5a15771643a0d0e49ab8b84692840c8873d4b7
Manuel Bugallo <marvinody@gmail.com>
2019-11-15 19:58:47 -0500

    feat: pulls commits latest first

--- src/store/commits.js
+++ src/store/commits.js
@@ -5,7 +5,7 @@ const LOAD_PICS = 'LOAD_PICS'
 export const getCommits = () => async dispatch => {
   const db = firebase.firestore();
   const commitsRef = db.collection('commits');
-  const snapshot = await commitsRef.limit(20).get()
+  const snapshot = await commitsRef.orderBy('date_committed', 'desc').limit(20).get()
   let commits = []
   snapshot.forEach(doc =>
     commits = [...commits, doc.data()]
