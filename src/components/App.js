638c0b7e04cb932ed211deea665eba91d8e8abde
Manuel Bugallo <marvinody@gmail.com>
2019-11-15 20:06:15 -0500

    feat: added header with source link

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Commits from './Commits';
 function App() {
   return (
     <div className="App">
+      <div className='header'>
+        <h1>3am Commits</h1>
+        <h3><a href='https://github.com/marvinody/3am-commits'>source</a></h3>
+      </div>
       <Commits></Commits>
     </div>
   );
