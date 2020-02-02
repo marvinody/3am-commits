9d8d1cef9aa2593039cca2169694c8e2a8967ca9
Manuel Bugallo <marvinody@gmail.com>
2020-02-02 02:35:36 -0500

    feat: updates analytic creation a bit more

--- functions/src/analytics.ts
+++ functions/src/analytics.ts
@@ -1,18 +1,33 @@
 import { bucket, getHour } from "./utils"
 import { Commit } from "./GithubApi"
 
+type analytic = {
+  curse: string
+  cursesOnHour: number[] // 0 -> 12am,... value is curses at that hour
+  cursesOnDay: number[] // 0 -> monday, ... value is curses on that day
+}
+
 type analytics = {
   date: number // unix timestamp
-  cursesAtHour: number[] // 0 -> 12am,... number is curses at that hour
+  data: analytic[]
 }
 
 export const computeAnalytics = (commits: Commit[]): analytics => {
   const timeBuckets = bucket(commits, commit =>
     Number(getHour(commit.date_committed))
   )
+  const dayBuckets = bucket(commits, commit =>
+    new Date(commit.date_committed).getDay()
+  )
 
   return {
     date: Date.now(),
-    cursesAtHour: timeBuckets.map(b => b.length),
+    data: [
+      {
+        curse: "fuck",
+        cursesOnHour: timeBuckets.map(b => b.length),
+        cursesOnDay: dayBuckets.map(b => b.length),
+      },
+    ],
   }
 }
